05de614efa640609f389e2f553eaf9c3f61c825d
Mira <57226653+Amirust@users.noreply.github.com>
2022-10-23 02:13:07 +0300

    я запушил проверяй
    - Добавлено отображение новой репутации при добавлении / отнимании

--- src/commands/context.addrep.js
+++ src/commands/context.addrep.js
@@ -42,6 +42,6 @@ module.exports =
 		await reputation.save();
 
 		bot.cooldowns.reputation.set( interaction.user.id, { createdAt: Date.now() } );
-		return interaction.reply({ content: locale.Success.format([ `<@${member.id}>` ]) });
+        return interaction.reply({ content: locale.Success.format([ `<@${member.id}>`, reputation.reputatio ]) });
 	}
 };
